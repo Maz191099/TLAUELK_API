a6aba966dcf07aafe2b0245e7bbf7f76a07fedb1
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-07-20 22:16:33 -0500

    registro de modelos en la app

--- commentary/admin.py
+++ commentary/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Commentary
+# Register your models here.61
 
-# Register your models here.
+class CommentaryAdmin(admin.ModelAdmin):
+    com_display = ('id','content', 'score')
+
+admin.site.register(Commentary, CommentaryAdmin)
