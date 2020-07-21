f4fb8cfb7efe6394ac164140814d1c26295b2b93
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-07-20 22:19:08 -0500

    configuracion de la funcion _str_

--- commentary/models.py
+++ commentary/models.py
@@ -6,3 +6,6 @@ class Commentary(models.Model):
     
     content = models.TextField()
     score = models.PositiveIntegerField()
+#Configuracion de la funcion __str__ 62
+    def __str__(self):
+        return self.content
