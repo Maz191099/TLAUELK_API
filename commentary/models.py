4381671fc89aa2bb21f85fdc8a08a6f8782fb72d
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-07-20 21:35:05 -0500

    creando modelos iniciales para comentarios

--- commentary/models.py
+++ commentary/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+# Create your models here.58
+#Creacion de modelos iniciales para comentarios de producto
+class Commentary(models.Model):
+    
+    content = models.TextField()
+    score = models.PositiveIntegerField()
