b1f5e02ddc9a6a6ba00a77c93b4d4d1e3cee0775
Jessica M. Hernandez <jessii.hdez.20@gmail.com>
2020-07-22 10:09:53 -0500

    modificacion en models

--- commentary/models.py
+++ commentary/models.py
@@ -1,12 +1,16 @@
+# Modelo de la rama jess (comentarios).
+
 from django.db import models
-#importar de productos
-# Create your models here.58
-#Creacion de modelos iniciales para comentarios de producto
+#importar de establecimientos
+#from establecimientos.models import Establishment
+
+#Creacion de modelos iniciales para comentarios de producto.58
 class Commentary(models.Model):
-    #
-    #
+    name = models.CharField(max_length=255, verbose_name="Nombre")
     content = models.TextField(verbose_name="Opinión")
     score = models.PositiveIntegerField(verbose_name="Estrellas")
+   # establecimiento = models.ForeignKey(Establishment, on_delete=models.CASCADE)
+
 #Configuracion de la funcion __str__ 62
     def __str__(self):
         return self.content
